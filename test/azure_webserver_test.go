3617c5bc71e80f5ceed9ed1cf1d4e84dada24ce3
Jonathan Martins <mart1449@algonquinlive.com>
2025-06-13 01:30:00 -0400

    Resolved TFLint issues and followed naming + documentation conventions

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "b3d019b0-c6fd-4f25-ac45-54f25745f266"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "mart1449",
 		},
 	}
 
